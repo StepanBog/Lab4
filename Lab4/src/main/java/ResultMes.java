d20241e9a7d75ec55e247250f2d742cf454b8e03
stepanbog <step300699@yandex.ru>
2019-12-04 22:17:39 +0300

    Scripted auto-commit on change (2019-12-04 22:17:39) by gitwatch.sh

--- Lab4/src/main/java/ResultMes.java
+++ Lab4/src/main/java/ResultMes.java
@@ -1,2 +1,11 @@
 public class ResultMes {
+    private Integer packegeID;
+    private String result;
+    private Test test;
+
+    public ResultMes(Integer packegeID, String result, Test test){
+        this.packegeID = packegeID;
+        this.result = result;
+        this.test = test;
+    }
 }
