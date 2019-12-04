4c6f111b25e49055800f21d868d6543e10c9d4a1
stepanbog <step300699@yandex.ru>
2019-12-04 21:47:01 +0300

    Scripted auto-commit on change (2019-12-04 21:47:01) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -1,8 +1,13 @@
 import akka.*;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.Props;
 
 public class Lab4 {
     public static void main(String[] args){
-        Props props1 = Props.create(StoreActor.class);
+        ActorSystem system = ActorSystem.create("lab4");
+        ActorRef storeActor = system.actorOf(
+                Props.create(StoreActor.class)
+        );
     }
 }
