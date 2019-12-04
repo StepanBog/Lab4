bbd1bd8fd5a501fbeb344365c2872cf88fef05ae
stepanbog <step300699@yandex.ru>
2019-12-04 21:56:13 +0300

    Scripted auto-commit on change (2019-12-04 21:56:13) by gitwatch.sh

--- Lab4/src/main/java/StoreActor.java
+++ Lab4/src/main/java/StoreActor.java
@@ -1,9 +1,10 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 public class StoreActor extends AbstractActor {
-    
+    private  Map<String, List<String>> 
     @Override
     public Receive createReceive() {
         return null;
