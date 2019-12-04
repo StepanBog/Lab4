d3d974cbfc3ecc170b1179ea0e3b18bf183803b3
stepanbog <step300699@yandex.ru>
2019-12-04 21:37:10 +0300

    Scripted auto-commit on change (2019-12-04 21:37:10) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -1,5 +1,6 @@
 public class Lab4 {
     public static void main(String[] args){
-        
+        Props props2 = Props.create(ActorWithArgs.class,
+                ● () -> new ActorWithArgs("arg"));
     }
 }
