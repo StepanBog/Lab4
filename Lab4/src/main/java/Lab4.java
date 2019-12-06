20f23eb6bf99518da11066eb101596f7f4421bd8
stepanbog <step300699@yandex.ru>
2019-12-06 18:04:35 +0300

    Scripted auto-commit on change (2019-12-06 18:04:35) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -1,6 +1,4 @@
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 
@@ -12,9 +10,9 @@ public class Lab4 {
         ActorSystem system = ActorSystem.create("lab4");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        ActorRef storeActor = system.actorOf(
-                Props.create(StoreActor.class)
-        );
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
 
     }
 }
