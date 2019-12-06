d754babb25d7c8b1fe4bfa89ec20dfce0ffc46de
stepanbog <step300699@yandex.ru>
2019-12-06 23:31:56 +0300

    Scripted auto-commit on change (2019-12-06 23:31:56) by gitwatch.sh

--- Lab4/src/main/java/RouterActor.java
+++ Lab4/src/main/java/RouterActor.java
@@ -1,12 +1,13 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class RouterActor extends AbstractActor{
     private ActorRef storeActor;
     private ActorRef testactor;
 
     public void RouterActor(){
-        this.storeActor = getContext().actorOf()
+        this.storeActor = getContext().actorOf(Props.create(StoreActor.class))
     }
     public AbstractActor.Receive createReceive() {
         return receiveBuilder()
