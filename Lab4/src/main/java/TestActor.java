0fe20af3204901f4cbea75a1bfe04e5e5310bbc9
stepanbog <step300699@yandex.ru>
2019-12-06 16:52:47 +0300

    Scripted auto-commit on change (2019-12-06 16:52:47) by gitwatch.sh

--- Lab4/src/main/java/TestActor.java
+++ Lab4/src/main/java/TestActor.java
@@ -1,5 +1,9 @@
 import akka.actor.AbstractActor;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.security.cert.CertPathBuilder;
 
 public class TestActor extends AbstractActor{
@@ -7,7 +11,10 @@ public class TestActor extends AbstractActor{
         return receiveBuilder()
                 .build();
     }
-    public void doTest(Test test){
-        
+    public void doTest(TestMes test) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(test.getJsScript());
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(test.getFunction(), test.getTest()params).toString();
     }
 }
