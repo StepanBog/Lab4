6904fdb392052ba6055fb784212294b9cab6acec
stepanbog <step300699@yandex.ru>
2019-12-06 17:49:54 +0300

    Scripted auto-commit on change (2019-12-06 17:49:54) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -2,10 +2,12 @@ import akka.*;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import jodd.io.http.Http;
 
 public class Lab4 {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("lab4");
+        final Http http = Http.get(system);
         ActorRef storeActor = system.actorOf(
                 Props.create(StoreActor.class)
         );
