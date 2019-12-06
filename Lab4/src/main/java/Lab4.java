c8a395b514b8e7353c311021d96f66dc72b337cd
stepanbog <step300699@yandex.ru>
2019-12-06 17:53:30 +0300

    Scripted auto-commit on change (2019-12-06 17:53:30) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -9,7 +9,7 @@ import akka.http.javadsl.*;
 public class Lab4 {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("lab4");
-        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
         MainHttp instance = new MainHttp(system);
         ActorRef storeActor = system.actorOf(
                 Props.create(StoreActor.class)
