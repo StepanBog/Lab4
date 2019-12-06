f0fde5050f919517d82aed806be07fae203acdaa
stepanbog <step300699@yandex.ru>
2019-12-06 17:59:51 +0300

    Scripted auto-commit on change (2019-12-06 17:59:51) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -10,8 +10,7 @@ im
 public class Lab4 {
     public static void main(String[] args){
         ActorSystem system = ActorSystem.create("lab4");
-        final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp(system);
+        
         ActorRef storeActor = system.actorOf(
                 Props.create(StoreActor.class)
         );
