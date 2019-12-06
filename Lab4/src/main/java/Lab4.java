7e122583324edaeb33da44a0b979950d3c8c4dce
stepanbog <step300699@yandex.ru>
2019-12-06 18:18:51 +0300

    Scripted auto-commit on change (2019-12-06 18:18:51) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -16,7 +16,7 @@ public class Lab4 {
         ActorSystem system = ActorSystem.create("lab4");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp();
+        MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute(system).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
