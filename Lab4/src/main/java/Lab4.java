b4adcb7964ea9719caa4cb5c3a3bcb7d9996716d
stepanbog <step300699@yandex.ru>
2019-12-06 23:24:14 +0300

    Scripted auto-commit on change (2019-12-06 23:24:14) by gitwatch.sh

--- Lab4/src/main/java/Lab4.java
+++ Lab4/src/main/java/Lab4.java
@@ -50,7 +50,7 @@ public class Lab4 {
         routerActor = system.actorOf(Props.create(RouterActor.class));
         return concat(get(() ->
             parameter("packageId",Id ->{
-            Future<Object> result = Patterns.ask(TestActor.class,
+            Future<Object> result = Patterns.ask(TestActor.class,Id
                     , 5000);
             return completeOKWithFuture(result, Jackson.marshaller());
         })),
