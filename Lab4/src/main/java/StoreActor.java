07be7fc1e27043c2b90fd7455cdd5e7c1dda8171
stepanbog <step300699@yandex.ru>
2019-12-11 14:53:04 +0300

    Scripted auto-commit on change (2019-12-11 14:53:04) by gitwatch.sh

--- Lab4/src/main/java/StoreActor.java
+++ Lab4/src/main/java/StoreActor.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class StoreActor extends AbstractActor {
-    private  Map<Integer, List<ResultTest>> storage = new HashMap<>();
+    private  Map<Integer, List<ResultMes>> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
@@ -17,9 +17,15 @@ public class StoreActor extends AbstractActor {
     }
 
     public void inputResult(ResultMes res){
-        List<ResultTest> list = new ArrayList<>();
-        list.add(res.getResTest());
-        storage.put(res.getPackageID(),list);
+        if (storage.containsKey(res.getPackageID())) {
+            List<ResultMes> list = storage.get(res.getPackageID());
+
+        }
+        else {
+            List<ResultTest> list = new ArrayList<>();
+            list.add(res.getResTest());
+            storage.put(res.getPackageID(), list);
+        }
     }
     public void goTest(TestMes test){
         sender().tell();
