9d06694dc5934cbaac382b7ce2e9e57c864fc5a7
stepanbog <step300699@yandex.ru>
2019-12-11 14:26:46 +0300

    Scripted auto-commit on change (2019-12-11 14:26:46) by gitwatch.sh

--- Lab4/src/main/java/RouterActor.java
+++ Lab4/src/main/java/RouterActor.java
@@ -22,7 +22,7 @@ public class RouterActor extends AbstractActor{
     private void runTest(TestPackage testPackage){
         for (Test test: testPackage.getTests()) {
             TestMes message = new TestMes (testPackage.getPackageID(),testPackage.getFuncName(),testPackage.getJsScript(), test);
-            
+            this.testactor
         }
     }
 }
