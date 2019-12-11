165f104452a7ae1e784d31e733d94a6846b35fbc
stepanbog <step300699@yandex.ru>
2019-12-11 15:13:46 +0300

    Scripted auto-commit on change (2019-12-11 15:13:46) by gitwatch.sh

--- Lab4/src/main/java/Test.java
+++ Lab4/src/main/java/Test.java
@@ -1,9 +1,11 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
     private String testName;
     private String expResult;
     private int[] params;
 
-    public Test(String testName,String expResult, int[] params){
+    public Test(@JsonProperty("packageID")String testName, String expResult, int[] params){
         this.expResult = expResult;
         this.params = params;
         this.testName = testName;
