5f5df1cd162aa12bf49e1c86e3af4e76d06f8db8
stepanbog <step300699@yandex.ru>
2019-12-11 14:11:47 +0300

    Scripted auto-commit on change (2019-12-11 14:11:47) by gitwatch.sh

--- Lab4/src/main/java/TestPackage.java
+++ Lab4/src/main/java/TestPackage.java
@@ -6,4 +6,8 @@ public class TestPackage {
     private String funcName;
     private Test[] tests;
 
+    public TestPackage(Integer packageID, String jsScript,String funcName, Test[] tests){
+        
+    }
+
 }
