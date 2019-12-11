68b688311fb0dd324ba842f00919a61df6edb053
stepanbog <step300699@yandex.ru>
2019-12-11 14:59:10 +0300

    Scripted auto-commit on change (2019-12-11 14:59:10) by gitwatch.sh

--- Lab4/src/main/java/RouterActor.java
+++ Lab4/src/main/java/RouterActor.java
@@ -18,7 +18,7 @@ public class RouterActor extends AbstractActor{
                 .build();
     }
     public void getTest(TestPackageMsg message){
-        getSender().tell(new TestAnswer( message.getPackageID(),));
+     this.storeActor.tell(message,);
 
     }
     private void runTest(TestPackage testPackage){
