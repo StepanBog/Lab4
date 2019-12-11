3e640349c01ef606a1d164c08d2c75115319eb85
stepanbog <step300699@yandex.ru>
2019-12-11 19:30:41 +0300

    Scripted auto-commit on change (2019-12-11 19:30:40) by gitwatch.sh

--- Lab4/src/main/java/TestAnswer.java
+++ Lab4/src/main/java/TestAnswer.java
@@ -7,5 +7,12 @@ public class TestAnswer {
         this.packageID = packageID;
         this.resultTests = resultTests;
     }
-    
+
+    public Integer getPackageID() {
+        return packageID;
+    }
+
+    public List<ResultTest> getResultTests() {
+        return resultTests;
+    }
 }
